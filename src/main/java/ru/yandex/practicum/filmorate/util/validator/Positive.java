ac3c8dc034ea39a5de6a441fd9c9d8d907afa851
Grigorii Balabanov <810411@mail.ru>
2022-06-02 19:57:12 +0500

    style: форматирование кода.

--- src/main/java/ru/yandex/practicum/filmorate/util/validator/Positive.java
+++ src/main/java/ru/yandex/practicum/filmorate/util/validator/Positive.java
@@ -1,9 +1,10 @@
 package ru.yandex.practicum.filmorate.util.validator;
+
 import javax.validation.Constraint;
 import javax.validation.Payload;
 import java.lang.annotation.*;
 
-@Target({ ElementType.FIELD })
+@Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = PositiveDurationValidator.class)
 @Documented
